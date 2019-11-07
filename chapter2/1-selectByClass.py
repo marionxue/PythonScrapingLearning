1f99dc631f74a0350b2c0ef777aa81ad8538c7db
marionxue <you@example.com>
2019-11-07 23:47:30 +0800

    修改parse build

--- chapter2/1-selectByClass.py
+++ chapter2/1-selectByClass.py
@@ -2,8 +2,9 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
-bsObj = BeautifulSoup(html, "html.parser")
+bsObj = BeautifulSoup(html, "lxml")
 
+# print(bsObj)
 nameList = bsObj.findAll("span",{"class":"green"})
 for name in nameList:
 	print(name.get_text())
